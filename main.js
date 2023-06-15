543a57a8a0921b7aaac81e2fb57504faafb05afa
saraoriola <saraoriola@outlook.com>
2023-06-15 13:02:56 +0200

    hasta aquí funciona :)

--- main.js
+++ main.js
@@ -8,6 +8,46 @@ const friendsNav = document.getElementById("friendsNav");
 const homeDiv = document.getElementById("home");
 const schoolDiv = document.getElementById("school");
 const workDiv = document.getElementById("work");
-const studylDiv = document.getElementById("study");
+const studyDiv = document.getElementById("study");
 const friendsDiv = document.getElementById("friends");
 
+//HIDE AND GO - (Empezamos a jugar con los EVENTOS)
+function hideViews() {
+    homeDiv.classList.add("hide");
+    schoolDiv.classList.add("hide");
+    workDiv.classList.add("hide");
+    studyDiv.classList.add("hide");
+    friendsDiv.classList.add("hide");
+  }
+
+function goHome() {
+    hideViews();
+    homeDiv.classList.remove("hide");
+  }
+
+function goSchool() {
+    hideViews();
+    schoolDiv.classList.remove("hide");
+  }
+
+function goWork() {
+    hideViews();
+    workDiv.classList.remove("hide");
+  }
+
+function goStudy() {
+    hideViews();
+    studyDiv.classList.remove("hide");
+  }
+
+function goFriends() {
+    hideViews();
+    friendsDiv.classList.remove("hide");
+  }
+
+//ADD EVENT - (Seguimos jugando con los EVENTOS-ACCIÓN) - OJO AL CSS
+homeNav.addEventListener("click", goHome);
+schoolNav.addEventListener("click", goSchool);
+workNav.addEventListener("click", goWork);
+studyNav.addEventListener("click", goStudy);
+friendsNav.addEventListener("click", goFriends);
