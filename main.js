45970d84bf1242f40c7b5523e5579d83e93b2b66
saraoriola <saraoriola@outlook.com>
2023-06-15 12:47:18 +0200

    variables ok

---
+++ main.js
@@ -0,0 +1,13 @@
+// VARIABLES - (¿Para qué? para traernos los ELEMENTOS)
+const homeNav = document.getElementById("homeNav");
+const schoolNav = document.getElementById("schoolNav");
+const workNav = document.getElementById("workNav");
+const studyNav = document.getElementById("studyNav");
+const friendsNav = document.getElementById("friendsNav");
+
+const homeDiv = document.getElementById("home");
+const schoolDiv = document.getElementById("school");
+const workDiv = document.getElementById("work");
+const studylDiv = document.getElementById("study");
+const friendsDiv = document.getElementById("friends");
+
